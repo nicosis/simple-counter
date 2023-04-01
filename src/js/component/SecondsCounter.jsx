8c47919a72092f9fe8a792c9212ae49215a6d3bb
nicosis <nicosis@gmail.com>
2023-04-01 14:36:57 +0000

    work done

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -18,16 +18,27 @@ const SecondsCounter = ({ seconds }) => {
     diezmiles = secString.charAt(secString.length - 5);
   } else if (secString.length > 5) {
     centMillar = secString.charAt(secString.length - 6);
-  } 
+  }
   return (
     <div className="contenedor">
-      <div className="digito icon">*</div>
-      <div className="digito">{centMillar}</div>
-      <div className="digito">{diezmiles}</div>
-      <div className="digito">{millares}</div>
-      <div className="digito">{centenas}</div>
-      <div className="digito">{decenas}</div>
-      <div className="digito">{unidades}</div>
+      <div className="panel shadow">
+        <i className="fa-solid fa-ellipsis fli-1"/>
+        <i className="fa-solid fa-ellipsis fli-2"/>
+        <i className="fa-solid fa-ellipsis fli-3"/>
+        <i className="fa-solid fa-ellipsis fli-4"/>
+        <i className="fa-solid fa-ellipsis fli-5"/>
+      </div>
+      <div className="digito shadow">{centMillar}</div>
+      <div className="digito shadow">{diezmiles}</div>
+      <div className="digito shadow">{millares}</div>
+      <div className="digito shadow">{centenas}</div>
+      <div className="digito shadow">{decenas}</div>
+      <div className="digito shadow">{unidades}</div>
+      <div className="panel shadow">
+        <a id='startBtn'><i className="fa-regular fa-circle-play panel-btn"></i></a>
+        <a id='stopBtn'><i className="fa-regular fa-circle-stop panel-btn"></i></a>
+        <a id='resetBtn'><i className="fa-solid fa-ban panel-btn"></i></a>
+      </div>
     </div>
   );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,41 @@ import Secondscounter from "./component/SecondsCounter.jsx";
 
 //render your react application
 let countSec = 0;
+let interval;
 
-setInterval(() => {
+const renderCounter = () => {
+  ReactDOM.render(
+    <Secondscounter seconds={countSec} />,
+    document.querySelector("#app")
+  );
+};
+
+const startInterval = () => {
+  if (!interval) {
+    interval = setInterval(() => {
+      countSec++;
+      renderCounter();
+    }, 1000);
+  }
+};
+
+const stopInterval = () => {
+  clearInterval(interval);
+  interval = null;
+};
+
+const resetCounter = () => {
+  countSec = 0;
+  renderCounter();
+};
+
+/* setInterval(() => {
   countSec++
   ReactDOM.render(<Secondscounter seconds={countSec}/>, document.querySelector("#app"));
-}, 1000);
+}, 1000); */
+
+renderCounter();
+
+document.querySelector("#startBtn").addEventListener("click", startInterval);
+document.querySelector("#stopBtn").addEventListener("click", stopInterval);
+document.querySelector("#resetBtn").addEventListener("click", resetCounter);
